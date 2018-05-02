aac1cef2072cd37fd5ce1da5a64ad3f173973fe2
marwolaethblack <a.unal677@gmail.com>
2018-05-02 19:49:29 +0200

    add put and delete routes for patient

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -3,42 +3,86 @@ const app = express();
 const router = express.Router();
 
 const routes = (Patient) => {
-    router.get('/api/patients', (req, res) => {
-        let page = req.query.page; 
+
+    //Requires page number in query.
+    router.get('/api/patients', async (req, res) => {
+        let page = req.query.page;
+        if (!page) {
+            res.status(400).send("Provide a page number in query");
+            return;
+        }
         page = parseInt(page)     // page number
         let limit = 50;   // number of records per page
         let offset = page * limit;
 
-        Patient.findAndCountAll({
-            limit: limit,
-            offset: offset,
-            order: [
-                ['lastName', 'DESC']
-            ]
-        }).then((data) => {
+        try {
+            let data = await Patient.findAndCountAll({
+                limit: limit,
+                offset: offset,
+                order: [
+                    ['lastName', 'DESC']
+                ]
+            });
             let pages = Math.ceil(data.count / limit);
             offset = limit * (page - 1);
             let patients = data.rows;
             res.status(200).json({ 'result': patients, 'count': data.count, 'pages': pages });
-        })
-            .catch(function (error) {
-                res.status(500).send('Internal Server Error');
-            });
+
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
     })
 
-    router.post('/api/patients', (req, res) => {
+    router.post('/api/patients', async (req, res) => {
+        console.log(req.body);
         const body = req.body;
-        Patient.create({ ...body })
-            .then(createdPatient => {
-                res.status(200).json(createdPatient);
-            })
-            .catch(e => {
-                res.status(500).send('Internal Server Error');
-            })
+        try {
+            let createdPatient = await Patient.create({ ...body });
+            res.status(200).json(createdPatient);
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+
+
+    router.put('/api/patients', async (req,res) => {
+        const p = req.body;
+        if (!p.id || Object.keys(p).length < 2) {
+            res.status(500).send("Internal server Error");
+            return;
+        }
+        try {
+            let numOfUpdated = await Patient.update(p, { where: {id: p.id}});
+            res.status(200).json(numOfUpdated);
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        
+    });
+
+    router.delete('/api/patients/:id', async (req, res) => {
+        const patientId = req.params.id;
+        try {
+            const nOfDeleted = await Patient.destroy({where: {id: patientId}});
+            res.status(200).json(nOfDeleted);
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
     });
 
     return router
 }
+
+
+
 
 
 
