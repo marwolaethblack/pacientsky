ef408ee16b43fa82c9dc07d097ff952b53fa2c64
marwolaethblack <a.unal677@gmail.com>
2018-05-03 21:51:41 +0200

    add delete patient

--- src/containers/PatientDetails/PatientDetails.js
+++ src/containers/PatientDetails/PatientDetails.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import GenericForm from '../../components/GenericForm/GenericForm';
+import GenericButton from '../../components/GenericButton/GenericButton';
 
 class PatientDetails extends Component {
 
     state = {
         patient: {},
-        loading: false
+        loading: false,
+        error : ""
     }
 
      getPatientDetails = async () => {
@@ -30,6 +32,13 @@ class PatientDetails extends Component {
         }
         catch(err) {
             console.log(err);
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    loading: false,
+                    error: "Patient not found"
+                }
+            })
         }
     }
 
@@ -100,18 +109,45 @@ class PatientDetails extends Component {
 
     }
 
+    deletePatient = async () => {
+        const { id } = this.props.match.params;
+        try {
+            let nOfDeleted = await axios.delete(`/api/patients/${id}`);
+            console.log(nOfDeleted.data);
+            if(nOfDeleted.data === 1) {
+                this.props.history.push('/');
+            }
+        }
+        catch(err) {
+          console.log(err);
+          this.setState(prevState => {
+              return {
+                  ...prevState,
+                  error: "Could not delete patient"
+              }
+          })
+        }
+        
+    }
+
     render() {
         let form = null;
         if(this.state.loading) {
             form = (<p>Loading...</p>)
-        } else {
+        } else if(!this.state.loading && !this.state.error.length) {
             const initialState = this.state.patient;
             form = (<GenericForm  config={this.formConfig} initialState={initialState} onSubmit={this.editPatient}/>);
+        } else {
+            form = (<p>{this.state.error}</p>);
         }
         return(
             <div className="header-margin">
              <h1 className="page-heading">Edit Patient</h1>
                 {form}
+            <h1 className="page-heading">Delete Patient</h1>
+            <div className="delete-button-container">
+                 <GenericButton onClick={this.deletePatient} className=" generic-button delete-button">Delete</GenericButton>
+            </div>
             </div>
         )
     }
